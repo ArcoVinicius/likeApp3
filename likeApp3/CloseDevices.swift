a5abe0cb4fd10563b73c3d759fcdabba0e7e0e7e
KoJoVe <JoaoVicente@MacBook-Pro-de-Joao-2.local>
2015-03-16 13:49:58 -0300

    Basic Interface

---
+++ likeApp3/CloseDevices.swift
@@ -0,0 +1,35 @@
+//
+//  CloseDevices.swift
+//  likeApp3
+//
+//  Created by Joao Nassar Galante Guedes on 16/03/15.
+//  Copyright (c) 2015 Vinícius Arcoverde. All rights reserved.
+//
+
+import UIKit
+
+class CloseDevices: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- likeApp3/ViewController.swift
+++ likeApp3/ViewController.swift
@@ -8,42 +8,58 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate {
     
-    
-    var photoImageView = UIImageView(frame: CGRectMake(40, 120, 180, 180))
-    
-    
+    var photoImageView = UIImageView()
+    var likeTableView = UITableView()
+    var dislikeTableView = UITableView()
+    var nameLabel = UILabel()
+    var sizeRect = UIScreen.mainScreen().applicationFrame
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        self.photoImageView.backgroundColor = UIColor.grayColor()
+        
+        self.view.backgroundColor = UIColor.whiteColor()
+        
+        photoImageView.frame = CGRectMake((sizeRect.size.width - 100)/2, 50, 100, 100)
+        photoImageView.backgroundColor = UIColor.grayColor()
+        
+        nameLabel.frame = CGRectMake((sizeRect.size.width - 300)/2, 170, 300, 50)
+        nameLabel.textAlignment = .Center
+        nameLabel.text = "Clique para mudar seu nome"
+        
+        var y: CGFloat = 170 + 50 + 30
+        var tablesHeight: CGFloat = sizeRect.size.height - y - 60
+        
+        dislikeTableView.frame = CGRectMake(0, y, sizeRect.size.width/2, tablesHeight)
+        likeTableView.frame = CGRectMake(sizeRect.size.width/2, y, sizeRect.size.width/2, tablesHeight)
+        
+        self.view.addSubview(nameLabel)
         self.view.addSubview(photoImageView)
-        }
+        self.view.addSubview(likeTableView)
+        self.view.addSubview(dislikeTableView)
+        
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func openPhotoLibrary(sender: AnyObject) {
-        
-        var photoPicker = UIImagePickerController()
-        photoPicker.delegate = self
-        photoPicker.sourceType = .PhotoLibrary
-        self.presentViewController(photoPicker, animated: true, completion: nil)
-    }
+//    @IBAction func openPhotoLibrary(sender: AnyObject) {
+//        
+//        var photoPicker = UIImagePickerController()
+//        photoPicker.delegate = self
+//        photoPicker.sourceType = .PhotoLibrary
+//        self.presentViewController(photoPicker, animated: true, completion: nil)
+//    }
+//    
+//    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject:AnyObject])
+//    {
+//        photoImageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+//        self.dismissViewControllerAnimated(false, completion: nil)
+//    }
     
-    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject:AnyObject])
-    {
-        photoImageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
-        self.dismissViewControllerAnimated(false, completion: nil)
-    }
-
-    @IBOutlet weak var textField: UITextField?
-    
-    class TableViewController: UIViewController {
-    var likes
-    }
 }
 
