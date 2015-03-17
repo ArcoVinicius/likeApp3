e7a76d8fc2ced1a0559109cfd41031f404d21a62
KoJoVe <JoaoVicente@MacBook-Pro-de-Joao-2.local>
2015-03-17 17:55:15 -0300

    Main Screen

---
+++ likeApp3/LikeDelegate.swift
@@ -0,0 +1,51 @@
+//
+//  LikeDelegate.swift
+//  likeApp3
+//
+//  Created by Joao Nassar Galante Guedes on 17/03/15.
+//  Copyright (c) 2015 Vinícius Arcoverde. All rights reserved.
+//
+
+import UIKit
+
+class LikeDelegate: NSObject, UITableViewDelegate, UITableViewDataSource {
+    
+    var interests:[String] = []
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as UITableViewCell
+        
+        cell.textLabel.text = interests[indexPath.row]
+        
+        return cell
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+       
+        return 1
+    }
+   
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return interests.count
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+        if(editingStyle == UITableViewCellEditingStyle.Delete)
+        {
+            interests.removeAtIndex(indexPath.row)
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Top)
+        }
+        
+    }
+    
+    func addItem(item: String) {
+        
+        interests.append(item)
+        
+    }
+
+}
--- likeApp3/ViewController.swift
+++ likeApp3/ViewController.swift
@@ -8,12 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UITextFieldDelegate {
+    
+    var addingLike = false
+    var addingDislike = false
+    
+    var likes = LikeDelegate()
+    var dislikes = LikeDelegate()
     
     var photoImageView = UIImageView()
+    
     var likeTableView = UITableView()
     var dislikeTableView = UITableView()
-    var nameLabel = UILabel()
+    
+    var likeAdd = UIButton()
+    var dislikeAdd = UIButton()
+    
+    var nameLabel = UITextField()
+    var likesText = UITextField()
+    
+    var likesView = UIView()
+    
     var sizeRect = UIScreen.mainScreen().applicationFrame
     
     override func viewDidLoad() {
@@ -29,17 +44,85 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate {
         nameLabel.textAlignment = .Center
         nameLabel.text = "Clique para mudar seu nome"
         
+        nameLabel.delegate = self
+        
+        likesView.frame = CGRectMake(0, 0, sizeRect.size.width, sizeRect.size.height/2)
+        likesView.backgroundColor = UIColor.whiteColor()
+        likesView.hidden = true
+        
         var y: CGFloat = 170 + 50 + 30
-        var tablesHeight: CGFloat = sizeRect.size.height - y - 60
+        var tablesHeight: CGFloat = sizeRect.size.height - y - 100
         
         dislikeTableView.frame = CGRectMake(0, y, sizeRect.size.width/2, tablesHeight)
         likeTableView.frame = CGRectMake(sizeRect.size.width/2, y, sizeRect.size.width/2, tablesHeight)
+        
+        likeTableView.delegate = likes
+        likeTableView.dataSource = likes
+        
+        dislikeTableView.delegate = dislikes
+        dislikeTableView.dataSource = dislikes
+        
+        dislikeAdd.frame = CGRectMake(((sizeRect.size.width/2)-100)/2, y + tablesHeight + 30, 100, 40)
+        likeAdd.frame = CGRectMake( (((sizeRect.size.width/2)-100)/2) + sizeRect.size.width/2 , y + tablesHeight + 30, 100, 40)
+        likeAdd.addTarget(self, action: "addLike", forControlEvents: .TouchUpInside)
+        dislikeAdd.targetForAction(Selector("addDislike"), withSender: nil)
+        
+        dislikeAdd.setTitle("Add", forState: .Normal)
+        likeAdd.setTitle("Add", forState: .Normal)
+        
+        dislikeAdd.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        likeAdd.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        
+        likesText.frame = CGRectMake((sizeRect.size.width - 200)/2, 50, 200, 40)
+        likesText.borderStyle = UITextBorderStyle.Line
+        
+        likesText.delegate = self
+        
+        likesView.addSubview(likesText)
         
         self.view.addSubview(nameLabel)
         self.view.addSubview(photoImageView)
         self.view.addSubview(likeTableView)
         self.view.addSubview(dislikeTableView)
+        self.view.addSubview(likeAdd)
+        self.view.addSubview(dislikeAdd)
+        self.view.addSubview(likesView)
         
+    }
+    
+    func addLike() {
+        addingLike = true
+        addingDislike = false
+        likesView.hidden = false
+        
+    }
+    
+    func addDislike() {
+        addingLike = true
+        addingDislike = false
+        likesView.hidden = false
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        if(addingLike == true) {
+            likes.addItem(likesText.text)
+            likeTableView.reloadData()
+        }
+        
+        if(addingDislike == true) {
+            dislikes.addItem(likesText.text)
+            dislikeTableView.reloadData()
+        }
+        
+        addingDislike = false
+        addingLike = false
+        
+        likesView.hidden = true
+        likesText.text = ""
+        
+        textField.resignFirstResponder()
+        return true;
     }
 
     override func didReceiveMemoryWarning() {
